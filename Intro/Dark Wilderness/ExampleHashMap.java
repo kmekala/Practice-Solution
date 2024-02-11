834ff8b3ef6db8750c770f7114321a356eb2218a
kalyan Mekala <mekala.kalyan@gmail.com>
2024-02-11 00:16:15 -0500

    Update ExampleHashMap.java

--- Intro/Dark Wilderness/ExampleHashMap.java
+++ Intro/Dark Wilderness/ExampleHashMap.java
@@ -3,36 +3,44 @@ import java.util.Map;
 
 public class ExampleHashMap {
     public static void main(String[] args) {
-        Map<String, Integer> myMap = new HashMap<>();
-
-        // isEmpty(): Checks if the map is empty
-        boolean isEmpty = myMap.isEmpty();
-        System.out.println("Is the map empty? " + isEmpty);
+        HashMap<String, Integer> myHashMap = new HashMap<>();
 
         // put(K key, V value): Associates the specified value with the specified key in the map
-        myMap.put("Apple", 1);
-        myMap.put("Banana", 2);
-        myMap.put("Orange", 3);
+        myHashMap.put("Apple", 1);
+        myHashMap.put("Banana", 2);
+        myHashMap.put("Orange", 3);
 
-        // size(): Returns the number of key-value pairs in the map
-        int size = myMap.size();
+        // size(): Returns the number of key-value mappings in the map
+        int size = myHashMap.size();
         System.out.println("Size of the map: " + size);
 
-        // containsKey(Object key): Checks if the map contains a specific key
-        boolean containsKey = myMap.containsKey("Banana");
-        System.out.println("Does the map contain the key 'Banana'? " + containsKey);
+        // get(Object key): Returns the value to which the specified key is mapped, or null if the key is not present
+        Integer value = myHashMap.get("Apple");
+        System.out.println("Value for key 'Apple': " + value);
 
-        // containsValue(Object value): Checks if the map contains a specific value
-        boolean containsValue = myMap.containsValue(3);
-        System.out.println("Does the map contain the value 3? " + containsValue);
+        // containsKey(Object key): Checks if the map contains the specified key
+        boolean containsKey = myHashMap.containsKey("Banana");
+        System.out.println("Does the map contain key 'Banana'? " + containsKey);
 
-        // get(Object key): Returns the value associated with a specified key
-        Integer value = myMap.get("Apple");
-        System.out.println("Value associated with the key 'Apple': " + value);
+        // containsValue(Object value): Checks if the map contains the specified value
+        boolean containsValue = myHashMap.containsValue(3);
+        System.out.println("Does the map contain value 3? " + containsValue);
 
-        // remove(Object key): Removes the key-value pair associated with the specified key from the map
-        myMap.remove("Banana");
+        // remove(Object key): Removes the mapping for the specified key from the map
+        Integer removedValue = myHashMap.remove("Orange");
+        System.out.println("Removed value for key 'Orange': " + removedValue);
 
-        System.out.println("Updated map: " + myMap);
+        // keySet(): Returns a set of all the keys in the map
+        System.out.println("Keys in the map: " + myHashMap.keySet());
+
+        // values(): Returns a collection of all the values in the map
+        System.out.println("Values in the map: " + myHashMap.values());
+
+        // entrySet(): Returns a set of all the key-value mappings in the map
+        System.out.println("Key-Value mappings in the map: " + myHashMap.entrySet());
+
+        // clear(): Removes all key-value mappings from the map
+        myHashMap.clear();
+        System.out.println("Map after clearing: " + myHashMap);
     }
 }
